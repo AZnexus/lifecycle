e08189aef2e0fc408ea9c57d47322677a01a050f
Alex Zarauza <azarauza@nexusgeographics.com>
2024-03-20 13:04:04 +0100

    intervals

--- src/app/products/pages/price/price.component.ts
+++ src/app/products/pages/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -10,8 +11,12 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   @Input()
   public price: number = 0;
 
+  public interval$?: Subscription; // EL $ es per indicar que es un Observable. O a vegades es una S majúscula.
+
   ngOnInit(): void {
     console.log('Componente HIJO: ngOnInit');
+
+    this.interval$ = interval(1000).subscribe(value => console.log(`Tick: ${value}`)); // Si no es tanca, anirà fent Ticks cada cop que es premi el botó de Toggle. Aixo es farà al OnDestroy()
   }
   ngOnChanges(changes: SimpleChanges): void {
     console.log('Componente HIJO: ngOnChanges');
@@ -19,5 +24,7 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   }
   ngOnDestroy(): void {
     console.log('Componente HIJO: ngOnDestroy');
+    this.interval$?.unsubscribe(); // Aixo cancela la subscripció del Observable. TAl i com està ara j ano hi haurà varis a la vegada a mesura que apretis el Toggle. Quan es torni a premer el Togge ja es farà el unsubscribe()
+    // Aquest lloc també es on s'hauria de destruir els eventListeners i similars.
   }
 }
